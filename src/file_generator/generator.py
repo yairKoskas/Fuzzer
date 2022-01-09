ac57fa21d187a017bda04bbe6d33466df92c5a92
HochItay <itayhoch1212@gmail.com>
2022-01-10 00:45:39 +0200

    add relatiom class and update generator interface

--- src/file_generator/generator.py
+++ src/file_generator/generator.py
@@ -1,23 +1,46 @@
 
+'''
+A class that holds information about relations.
+'''
+class Relation:
+
+    def __init__(self, type_of_relation, target_name) -> None:
+        self.type = type_of_relation
+        self.target = target_name
 
 '''
 An interface for generator.
-A generator can generate bytes arbitrary or by some rules, 
-it can also use other generators to generate data recursivly.
-Support generating valid data and also mutating it.
+A generator can represents an element in the template format XMl file.
+Can return Field class which holds actual value.
 '''
 class Generator:
-    def __init__(self) -> None:
+    def __init__(self, name=None) -> None:
+        self._relation = None
+        self._has_relation = False
+        self._name = name
+
+    '''
+    return field object with a valid value.
+    '''
+    def get_field(self):
         pass
 
     '''
-    Get valid value.
+    return True if the object has a relation
     '''
-    def valid_value(self) -> bytes:
-        pass
+    def has_relation(self):
+        return self._has_relation
 
     '''
-    Get (maybe) invalid value.
+    set the relation of the generator
     '''
-    def invalid_value(self) -> bytes:
-        pass
+    def set_relation(self, relation: Relation):
+        self._has_relation = True
+        self._relation = relation
+
+    '''
+    get the relation of the generator
+    '''
+    def get_relation(self, relation: Relation):
+        if self._has_relation:
+            return self._relation
