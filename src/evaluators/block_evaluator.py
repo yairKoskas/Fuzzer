d06635302ca1403fd924f17e0ec24c98f8494a50
yairKoskas <yairi2003@gmail.com>
2022-03-21 20:02:19 +0200

    interface reformatting
    interface reformatting

--- src/evaluators/block_evaluator.py
+++ src/evaluators/block_evaluator.py
@@ -4,10 +4,10 @@ from typing import Set
 
 class BlockEvaluator(CoverageEvaluator):
 
-    def __init__(self, program):
-        super().__init__(program)
+    def __init__(self):
+        super().__init__()
 
-    def get_coverage(self, inp) -> Set:
+    def get_coverage(self, path, inp) -> Set:
         super().get_coverage(inp)
     
     def attach_and_get_coverage(self, pid) -> Set:
