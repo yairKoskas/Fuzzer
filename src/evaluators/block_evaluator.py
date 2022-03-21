9046385e87c46ed3c329be0b7c24d6fb4e088cfa
yairKoskas <yairi2003@gmail.com>
2022-03-21 19:45:48 +0200

    added initial skeleton for edge and block coverage evaluators

---
+++ src/evaluators/block_evaluator.py
@@ -0,0 +1,14 @@
+from coverage_evaluator import CoverageEvaluator
+from typing import Set
+
+
+class BlockEvaluator(CoverageEvaluator):
+
+    def __init__(self, program):
+        super().__init__(program)
+
+    def get_coverage(self, inp) -> Set:
+        super().get_coverage(inp)
+    
+    def attach_and_get_coverage(self, pid) -> Set:
+        super().attach_and_get_coverage(pid)
