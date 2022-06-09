4f02a860db21b8a89e8e2c2b63a878236a7a2d76
yairKoskas <yairi2003@gmail.com>
2022-06-09 16:02:21 +0300

    switched to getting coverage via interscript messaging

--- src/evaluators/block_evaluator.py
+++ src/evaluators/block_evaluator.py
@@ -3,32 +3,57 @@ from utils.drcov import parse_coverage
 from typing import Set
 import frida
 import time
+import os
+
+modules = []
+bbs = []
+
+
+def check_pid(pid):
+    """ Check For the existence of a unix pid. """
+    try:
+        os.kill(pid, 0)
+    except OSError:
+        return False
+    else:
+        return True
+
+
+def on_message(msg, data):
+    payload = msg['payload']
+    global modules
+    global bbs
+    if 'map' in payload:
+        modules.append(payload['map'])
+    else:
+        bbs.append(data)
 
 
 class BlockEvaluator(CoverageEvaluator):
 
     def __init__(self, program):
         super().__init__(program)
-        with open('src/evaluators/scripts/frida_block_coverage.js', 'r') as f:
+        self.frida_device = None
+        self.frida_script = None
+        self.frida_session = None
+        self.pid = -1
+        with open('src/evaluators/scripts/frida_coverage.js', 'r') as f:
             self.script_code = f.read()
 
     def get_coverage(self, path, args, timeout) -> Set:
-        self.pid = frida.spawn(path, args)
-        self.frida_session = frida.attach(self.pid)
+        self.frida_device = frida.get_device('local')
+        self.pid = self.frida_device.spawn(path, [path] + args)
+        self.frida_session = self.frida_device.attach(self.pid)
         self.frida_script = self.frida_session.create_script(self.script_code)
+        self.frida_script.on('message', on_message)
         self.frida_script.load()
-        self.frida_script.exports.clearcoverage()
-        self.frida_script.exports.settarget()
         self.frida_session.resume()
         start = time.time()
-        while time.time() - start < timeout:
-            stalker_attached, stalker_finished = self.frida_script.exports.checkstalker()
-        if not stalker_finished:
-            print("Stalker didn't finish after timeout, couldn't get coverage")
-
-            # todo check if crashed, if it crashed, return the exit code, else return 1
-
-        coverage_data = self.frida_script.exports.getcoverage()
+        while check_pid(self.pid) and time.time() - start < timeout:
+            pass
+        self.frida_session.detach()
+        coverage_data = bbs
+        print(modules)
         print(coverage_data)
-        return parse_coverage(coverage_data)  # , exitcode
-
+        return 1
+        # return parse_coverage(coverage_data)  # , exitcode
