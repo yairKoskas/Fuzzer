2e94b966f02c58d58d812407045de436c93b4b7a
yairKoskas <yairi2003@gmail.com>
2022-06-23 13:10:10 +0300

    to add support in ghidra via ghidrabridge

--- src/factories/coverage_evaluator_factory.py
+++ src/factories/coverage_evaluator_factory.py
@@ -1,10 +1,11 @@
 from evaluators.block_evaluator import BlockEvaluator
-from evaluators.edge_evaluator import EdgeEvaluator
+# from evaluators.edge_evaluator import EdgeEvaluator
 class CoverageEvaluatorFactory:
     def create_evaluator(type: str, program: str):
         if type == "block":
             return BlockEvaluator(program)
         elif type == "edge":
-            return EdgeEvaluator(program)
+            pass
+            # return EdgeEvaluator(program)
         else:
             raise ValueError('No such coverage type')
